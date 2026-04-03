483e1d47529fda1aa49ab4217b3e2e5798a9b323
karentrasporte <karentrasporte@gmail.com>
2026-04-03 11:33:19 +0800

    Created backend code to call the weather API, and update main.py to remove temporary function

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
 import streamlit as st
+import plotly.express as px
+import backend
 
 st.title("Weather Forecast for the Next Days")
 place = st.text_input("Place: ")
 days = st.slider("Forecast Days", min_value=1, max_value=5, help="Select number of days to forecast")
 option = st.selectbox("Select data to view", ("Temperature", "Sky"))
 st.subheader(f"{option} for the next {days} in {place}")
+
+data = backend.get_data(place, days, option)
+
+figure = px.line(x=dates, y=temperatures, labels={"x": "Date", "y": "Temperature (C)"})
+st.plotly_chart(figure)
